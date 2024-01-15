58d1f0562122acac818f5a64aa6d690631a6b65c
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-01-15 10:25:24 -0300

    programa principal modificado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,5 +10,14 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+
+
+        System.out.println(d08);
+        System.out.println("d04 = " + d04.format(fmt1));
+
+        }
+
     }
-}
